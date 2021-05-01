11fc903a5afafe5f2122b6ad06216c07f0cf082a
HailayLin <hailay@qq.com>
2021-05-01 14:55:50 +0800

    重写冒泡排序：书上的实现，for循环循环的次数更加清晰，但是arr[0]不能用到，要多开一位，属于小瑕疵

--- sort/02_bubble_sort.cpp
+++ sort/02_bubble_sort.cpp
@@ -8,7 +8,7 @@
  * 
  */
 
-//重写冒泡排序：以前冒泡排序的实现
+//重写冒泡排序：书上的实现，for循环循环的次数更加清晰，但是arr[0]不能用到，要多开一位，属于小瑕疵
 
 #include<iostream>
 using namespace std;
@@ -17,14 +17,14 @@ int main(){
     int n;
     cin >> n;   //输入要排序的数个数
     
-    double *arr = new double(n); //动态创建数组
+    double *arr = new double(n+1); //动态创建数组
     //输入待排序数字
-    for (int i=0; i<n; i++) {
+    for (int i=1; i<=n; i++) {
         cin >> arr[i];
     }
     //排序,从大到小，小的在后面，不满足就调换
-    for (int i=0; i<n-1; i++) { //n个数只要排n-1趟，最后一个就自己归位了
-        for (int j=0; j<n-i-1; j++) {
+    for (int i=1; i<=n-1; i++) { //n个数只要排n-1趟，最后一个就自己归位了
+        for (int j=1; j<=n-i; j++) {
             if (arr[j]<arr[j+1]){
                 double tmp = arr[j+1];
                 arr[j+1] = arr[j];
@@ -33,7 +33,7 @@ int main(){
         }
     }
     //输出
-    for (int i=0; i<n; i++) {
+    for (int i=1; i<=n; i++) {
         cout << arr[i] << " ";
     }
 }
