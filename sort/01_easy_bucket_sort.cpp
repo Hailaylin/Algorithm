6d606e2d010a3cb2033632db8574f75c68db2a13
HailayLin <hailay@qq.com>
2021-05-01 14:17:12 +0800

    好玩的简易桶排序，自己理解实现超快乐

--- sort/01_easy_bucket_sort.cpp
+++ sort/01_easy_bucket_sort.cpp
@@ -8,26 +8,29 @@
  * 
  */
 
+//好玩的简易桶排序，自己理解实现超快乐
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 int main(){
     //实现5个数的简易桶排序,数字范围0-10
-    int array[11]={0};  
+    int array[11]={0};  //①这样初始化也行，默认初始化为0
     int tmp=0;
-    //初始化数组
+    //②遍历初始化数组
     for (int i=0; i<11; i++) {
         array[i]=0;
     }
+    //③调库初始化数组
+    memset(array, 0, sizeof(array));
     //输入并把数放入捅中
     for (int i=0; i<5; i++){
         cin >> tmp;
         array[tmp]++;
     }
     //从大到小输出
-    for (int i=11; i>=0; i--){
-        if (array[i]){  //实现输出，但输出会合并
+    for (int i=11; i>=0; i--){ //i,从数字0-11遍历
+        for (int j=array[i]; j>0; j--)       //标记每个数字输出多少次
             cout << i << " " ;
-        }
     }
 }
