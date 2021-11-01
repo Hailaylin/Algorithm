d1f05f6935a4179c0ee9f82490e2a259bf343dde
HailayLin <hailay@qq.com>
2021-11-01 11:17:24 +0800

    作业完成，测试完成

--- exp3.cc
+++ exp3.cc
@@ -2,9 +2,14 @@
  * @Description: 实验3 先序遍历创建二叉树
  * @Author: HailayLin
  * @Date: 2021-11-01 10:27:52
- * @LastEditTime: 2021-11-01 10:46:42
+ * @LastEditTime: 2021-11-01 11:16:43
  * @FilePath: \Algorithm\exp3.cc
  */
+/**
+ * 测试用例
+ * 1. ABC##DE#G##F###，节点7，深度5，叶子节点3
+ * 2. AB#CD##E##F#GH###，节点数8，深度4，叶子结点3
+ * */
 
 #include<iostream>
 using namespace std;
@@ -64,14 +69,15 @@ void PostOrderTraverse(BiTree &T)
     }
 }
 
+// 获取树深度
 int Depth(BiTree &T)
 {
     int m=0, n=0;
     if(T == NULL) return 0;
     else {
-        m = Depth(T->lchild);
-        n = Depth(T->rchild);
-        if(m>n) return (m+1);
+        m = Depth(T->lchild);   // 左子树深度
+        n = Depth(T->rchild);   // 右子树深度
+        if(m>n) return (m+1);   // 选择最深的那一边+1
         else return(n+1);
     }
 }
@@ -80,21 +86,58 @@ int Depth(BiTree &T)
 int NodeCount(BiTree &T)
 {
     if(T == NULL) return 0;
-    else return NodeCount(T->lchild)+NodeCount(T->rchild)+1;
+    else return NodeCount(T->lchild)+NodeCount(T->rchild)+1;    // 左子树个数+右子树+1
+}
+
+// 叶子结点个数
+int LeaveNodeCount(BiTree &T)
+{
+    if(T == NULL) return 0; // 树空返回0
+    if(T->lchild == NULL && T->rchild == NULL) return 1; // 是叶子结点就+1
+    else return LeaveNodeCount(T->lchild)+LeaveNodeCount(T->rchild);
+}
+
+// 选择输出菜单
+void SwitchShow(BiTree &T)
+{
+    int n;
+    do {
+    cout << "0退出，1先序遍历，2中序遍历，3后序遍历:";
+    cin >> n;
+        switch (n)
+        {
+        case 1:
+            cout << "前序遍历输出:" << endl; 
+            PreOrderTraverse(T); cout<<endl;//先序遍历
+            break;
+        case 2:
+            cout << "中序遍历输出:" << endl; 
+            InOrderTraverse(T); cout<<endl;//中序遍历
+            break;
+        case 3:
+            cout << "后序遍历输出:" << endl; 
+            PostOrderTraverse(T); cout<<endl;//后序遍历
+            break;
+        case 0:
+            break;
+        default:
+            cout << "您的输入有误，请重新输入." << endl;
+            break;
+        }
+    }while(n!=0);
 }
 
 int main()
 {
     BiTree T;
+    cout << "请输入前序遍历二叉树:";
     CreateBiTree(T);//先序遍历顺序建立二叉链表
-    cout << "前序遍历输出:" << endl; 
-    PreOrderTraverse(T); cout<<endl;//先序遍历
-    cout << "中序遍历输出:" << endl; 
-    InOrderTraverse(T); cout<<endl;//中序遍历
-    cout << "后序遍历输出:" << endl; 
-    PostOrderTraverse(T); cout<<endl;//后序遍历
+    
+    SwitchShow(T);// 菜单输出前序、中序、后序遍历；
+
     cout << "二叉树节点个数:" << NodeCount(T) << endl;
     cout << "二叉树深度个数:" << Depth(T) << endl;
+    cout << "二叉树叶子结点个数:" << LeaveNodeCount(T) << endl;
     
     return 0;
 }
