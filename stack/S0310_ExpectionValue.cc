0a6b02c357c9794a681fb6783ce659abafe44fbc
HailayLin <hailay@qq.com>
2021-11-13 16:10:17 +0800

    emm

--- stack/S0310_ExpectionValue.cc
+++ stack/S0310_ExpectionValue.cc
@@ -2,7 +2,7 @@
  * @Description: 表达式求值
  * @Author: HailayLin
  * @Date: 2021-11-12 16:47:01
- * @LastEditTime: 2021-11-12 16:49:18
+ * @LastEditTime: 2021-11-13 16:10:12
  * @FilePath: \Algorithm\stack\S0310_ExpectionValue.cc
  */
 /**
@@ -25,16 +25,27 @@ int main()
     return 0;
 }
 */
+
 #include<iostream>
 #include<string>
 #include<stack>
 using namespace std;
 
 
-
-void EvalueateExpression(string str)
+/**
+ * @brief 计算表达式的值
+ * 
+ * @param str 需要运算表达式，比如 #3*(7-2)# ,以#为表达式开始、结束符
+ * @return char 返回ANSII码表示的字符
+ */
+char EvalueateExpression(string str)
 {
+    stack<char> OPTR;   // 寄存运算符
+    stack<int> OPND;    // 寄存操作数或运算结果
+    OPTR.push('#');
+    char ch = str[0];
 
+    
 }
 
 int main()
