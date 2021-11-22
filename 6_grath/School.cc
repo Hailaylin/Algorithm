ca0c32e8fe305a7325b890db11044dfd77e71bd6
HailayLin <hailay@qq.com>
2021-11-22 11:18:05 +0800

    时间不够啊

--- 6_grath/School.cc
+++ 6_grath/School.cc
@@ -1,62 +1,10 @@
 /*
- * @Description: 
+ * @Description: 校园导航
  * @Author: HailayLin
- * @Date: 2021-11-15 10:37:10
- * @LastEditTime: 2021-11-22 10:26:57
- * @FilePath: \Algorithm\6_grath\S0602.cc
+ * @Date: 2021-11-22 10:22:23
+ * @LastEditTime: 2021-11-22 11:13:42
+ * @FilePath: \Algorithm\6_grath\School.cc
  */
-
-/**
- * 题号：S0602        题目:无向图的广度优先搜索遍历（邻接矩阵）        得分：0  
-  
-作业提交截止时间：2021/11/15 12:00:00	 
- 	
-题目内容：
-在S0305与S0601的基础上修改程序，使用邻接矩阵完成无向图的广度优先搜索遍历。
-
-例1（最后2行为输出，其他为输入）：
-8 8
-ABCDEFGH
-AF 1
-BA 2
-BC 3
-CG 4
-BD 5
-BE 6
-GH 7
-DH 8
-ABFCDEGH
-BACDEFGH
-
-例2（最后2行为输出）：
-5 4
-BACDE
-AB 1
-BC 2
-BD 3
-DE 4
-BACDE
-ABCDE
-
-例3（最后1行为输出）：
-1 0
-A
-A
-
-请注意，main()函数必须按如下所示编写：
-int main()
-{
-    AMGraph G;
-    CreateUDN(G);  //创建无向图的邻接矩阵 
-    BFS(G,0); //广度优先搜索遍历图 
-    cout<<endl;
-    for(int i=0;i<G.vexnum;i++) visited[i]=0;
-    if(G.vexnum>1) BFS(G,1); 
-    cout<<endl;
-    return 0;
-}
-
-*/
 
 #include<iostream>
 #include<queue>
@@ -115,7 +63,7 @@ Status CreateUDN(AMGraph &G)
     for(int i=0; i<G.vexnum; i++)
         for(int j=0; j<G.vexnum; j++)
         {
-            G.arcs[i][j]=0;
+            G.arcs[i][j]=MaxInt;
         }
 
    // cout << "// 3.构建邻接矩阵"<< endl;
@@ -163,7 +111,7 @@ void DFS_AM(AMGraph G, int v)
     // 递归深度遍历
     for(int w=0; w<G.vexnum; w++)
     {
-        if( G.arcs[v][w]!=0 && !visited[w])
+        if( G.arcs[v][w]!=MaxInt && !visited[w])
             DFS_AM(G, w);
     }
 }
@@ -188,7 +136,7 @@ void BFS(AMGraph G, int v)
         // 2.2 依次检查u的所有邻接点，若没访问过，就访问w,设置已访问，w入队
         for (int w = 0; w < G.vexnum; w++)
         {
-            if (G.arcs[u][w] != 0 && !visited[w])
+            if (G.arcs[u][w] != MaxInt && !visited[w])
             {
                 cout << G.vexs[w];
                 visited[w] = true;
@@ -196,6 +144,72 @@ void BFS(AMGraph G, int v)
             }
         }
     }
+}
+
+
+void Show_DIJ(int Path[], int v0)
+{
+    if (Path[v0] == -1) cout << "无最短路径" << endl;
+    int vi = v0;
+    while (Path[vi])
+    {
+        printf("Path[%d]=%d ——> ", vi, Path[vi]);
+        vi = Path[vi];
+    }
+    printf("\n");
+}
+
+
+/**
+ * @brief Dijkstra算法 求单源最短路径
+ * 
+ * @param G 
+ * @param v0 顶点
+ */
+void ShortestPath_DIJ(AMGraph G, int v0)
+{
+    // 1. 初始化
+    int n = G.vexnum;
+    bool S[n] = {false};
+    int Path[MVNum] = {0};
+    int D[MVNum] = {0};
+    int v ;
+    for (v = 0; v < n; v++)
+    {
+        S[v] = false;
+        D[v] = G.arcs[v0][v];
+        if (D[v] < MaxInt) Path[v] = v0;    // 有弧：v前驱为v0，下标
+        else Path[v] = -1;  // 无弧 -1
+    }
+    S[v0] = true;
+    D[v0] = 0;
+
+    // 2. 每次求得v0到某个顶点v的最短路径，将v加到S集
+    for (int i = 0; i < n; i++)
+    {
+        int min = MaxInt;
+        for (int w = 0; w < n; w++)
+        {
+            if (!S[w] && D[w] < min)
+            {
+                v = w;
+                min = D[w];
+            }
+        }
+
+        S[v] = true;
+        for (int w = 0; w < n; w++)
+        {
+            if (!S[w] && (D[v] + G.arcs[v][w] < D[w]))
+            {
+                D[w] = D[v] + G.arcs[v][w];
+                Path[w] = v;
+            }
+        }
+    }
+
+    // 3. 输出最短路径
+    Show_DIJ(Path, v0);
 }
 
 
@@ -208,5 +222,25 @@ int main()
     for(int i=0;i<G.vexnum;i++) visited[i]=0;
     if(G.vexnum>1) BFS(G,1); 
     cout<<endl;
+    cout << "输入要查看的最短路径的源点：" << endl;
+    int n;
+    cin >> n; 
+    ShortestPath_DIJ(G, n);
     return 0;
 }
+
+/**
+ * @brief 测试用例
+ * 
+8 8
+ABCDEFGH
+AF 1
+BA 2
+BC 3
+CG 4
+BD 5
+BE 6
+GH 7
+DH 8
+ * 
+ */
