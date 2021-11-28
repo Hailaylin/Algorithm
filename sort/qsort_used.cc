df39408056f5519961b1c7c14b9a60e95434dad6
HailayLin <hailay@qq.com>
2021-11-28 16:41:39 +0800

    加些备注

--- sort/qsort_used.cc
+++ sort/qsort_used.cc
@@ -2,13 +2,17 @@
  * @Description: 练习使用qsort
  * @Author: HailayLin
  * @Date: 2021-11-28 16:28:51
- * @LastEditTime: 2021-11-28 16:37:45
+ * @LastEditTime: 2021-11-28 16:40:30
  * @FilePath: \Algorithm\sort\qsort_used.cc
  */
 
 #include<iostream>
 using namespace std;
 
+/* 虽然能改变数据类型，但是不同的数据类型得单独判断
+参考
+https://blog.csdn.net/zhao888789/article/details/79186619
+*/
 typedef int ElemType;
 
 ElemType score[] = { 100, 88, 99, 10, 25, 61, 60};
@@ -19,9 +23,11 @@ ElemType score[] = { 100, 88, 99, 10, 25, 61, 60};
  * @param a 
  * @param b 
  */
-int cmpfunc(const void *a, const void *b)
+int cmpfunc(const void *_a, const void *_b)
 {
-    return ( *(ElemType *)a - *(ElemType *)b);
+    ElemType *a = (int *)_a;
+    ElemType *b = (int *)_b;
+    return *a - *b;
 }
 
 // 输出数组元素
