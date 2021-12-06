9e396d83893e07ed28ff21b8f8545f94f9092c8b
HailayLin <hailay@qq.com>
2021-12-06 16:07:19 +0800

    + clion

--- ClassDesign/main.cpp
+++ ClassDesign/main.cpp
@@ -1,22 +1,9 @@
-/*
- * @Description: 指针的复习
- * @Author: HailayLin
- * @Date: 2021-12-06 09:14:02
- * @LastEditTime: 2021-12-06 13:57:10
- * @FilePath: \ClassDesign\1cPointer.cc
- */
-
 #include<iostream>
 using namespace std;
 
 int sum(int a, int b)
 {
     return a+b;
-}
-
-void DayByDay()
-{
-    return ;
 }
 
 int main()
@@ -27,7 +14,6 @@ int main()
     pointer = &x;
     printf("x=%d\n", x);
     printf("pointer=%p\n", pointer);
-    printf("pointerAddr=%p\n", &pointer);
     printf("pointerVal=%d\n", *pointer);
 
     // 指向函数的指针
@@ -36,13 +22,5 @@ int main()
     printf("sumAddr=%p\t p=%p\n", &sum, p);
     printf("p(3,4)=%d\n", p(3,4));
 
-    // 五行代碼
-    long long TimePassPy=1;
-    bool 鐘意=1;
-    while(TimePassPy){
-        DayByDay();
-        if(鐘意) continue;
-        if(!鐘意) break;
-    }
-}
 
+}
